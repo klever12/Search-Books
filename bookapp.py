0c90414af951d45529885f5cb6a88966bedac7e8
Klever <kgopar20@wooster.edu>
2021-06-13 20:11:09 -0500

    implemented the start_app method

--- bookapp.py
+++ bookapp.py
@@ -13,8 +13,42 @@ class BookApp:
 
     def start_app(self):
         """This method starts the application"""
-        # TODO
-        print("App started!")
+
+        print("Welcome to my book search app! \n")
+
+        # Starts the application
+        while self.app_quit is not True:
+            user_choice = input("Would you like to search for a book(b), "
+                                "see your reading list(r), or quit app(q)?\n")\
+                .strip()
+
+            if user_choice == "B" or user_choice == "b":
+                # calls search_books method to search for books
+                self.search_books()
+
+            elif user_choice == "R" or user_choice == "r":
+                # prints the user's reading list if not empty
+                curr_reading_list = self.get_reading_list()
+
+                if bool(curr_reading_list) is False:
+                    print("Reading list is currently empty.\n")
+                else:
+                    print("Here is your current reading list: \n")
+                    for x in curr_reading_list:
+                        authors = ", ".join(curr_reading_list[x][1])
+                        print(str(x) + " Title: " + curr_reading_list[x][0] +
+                              ", Author(s): " + authors +
+                              ", Publishing Company: " +
+                              curr_reading_list[x][2] + "\n")
+
+            elif user_choice == "Q" or user_choice == "q":
+                # sets self.app_quit to True to exit while loop and quit app
+                self.app_quit = True
+
+            else:
+                print("Sorry. That was an invalid option.\n")
+
+        print("Goodbye!")
 
     def get_reading_list(self):
         """This method returns the reading list"""
@@ -29,12 +63,12 @@ class BookApp:
     def search_books(self):
         """This method searches for books using the Google Books API"""
         # TODO
-        print("Performed book search!")
+        print("Performed book search!\n")
 
     def add_to_reading_list(self):
         """This method adds books the user chooses to the reading list"""
         # TODO
-        print("Added books to reading list!")
+        print("Perform add books to reading list!\n")
 
 
 if __name__ == '__main__':
